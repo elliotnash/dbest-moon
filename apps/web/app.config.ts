1384314c0f468e9c6914189725440d432bc3d628
Elliot Nash <elliotnash@gmail.com>
2024-07-21 16:12:10 -0700

    Fix hydration error

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "@solidjs/start/config";
-import tsconfigPaths from "vite-tsconfig-paths";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   vite: {
     plugins: [tsconfigPaths()]
-  },
-  solid: {
-    
   }
 });
