9110aaac7afef18e8bbd737058d8b8725fc87489
Elliot Nash <elliotnash@gmail.com>
2024-07-24 16:05:29 -0700

    Add openapi docs

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,61 +1,60 @@
-import { Elysia, t, getSchemaValidator } from "elysia";
+import { Elysia, t } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { createId } from '@paralleldrive/cuid2';
 import env from "./env";
 import { db } from "./database/db";
 import { clicks } from "./database/schema";
 import { count, eq } from "drizzle-orm";
+import swagger from "@elysiajs/swagger";
+import { ThemeId } from "@elysiajs/swagger/scalar/types";
 
 const app = new Elysia()
+  .use(swagger({
+    path: "docs",
+    scalarConfig: {
+      theme: "kepler" as ThemeId
+    }
+  }))
   .use(cors())
-  .get("/greeting", () => "Hello Elysia!" as const, {
-    response: t.Literal("Hello Elysia!")
-  })
-  .put("/click", async ({ cookie: { deviceId } }) => {
-    // Ensure device id cookie is set
-    if (!deviceId.value) {
-      deviceId.set({
-        value: createId(),
-        path: "/"
-      });
-      console.log(`created deviceId in click ${deviceId}`);
-    }
-    deviceId.expires = new Date(Date.now() + 365 * 24 * 60 * 60 * 1000);
-    
-    await db.insert(clicks).values({
-      deviceId: deviceId.value!,
-      time: new Date()
-    });
-  },
-  {
-    cookie: t.Cookie({
-      deviceId: t.Optional(t.String())
-    }),
-    response: t.Void()
-  })
-  .get("/clicks", async ({ cookie: { deviceId } }) => {
-    // Ensure device id cookie is set
-
-    if (!deviceId.value) {
-      deviceId.set({
-        value: createId(),
-        path: "/"
-      });
-      console.log(`created deviceId in clicks ${deviceId}`);
-    }
-    deviceId.expires = new Date(Date.now() + 365 * 24 * 60 * 60 * 1000);
-
-    const select = await db.select({ count: count() })
-      .from(clicks)
-      .where(eq(clicks.deviceId, deviceId.value!));
-    return select[0].count;
-  },
-  {
-    cookie: t.Cookie({
-      deviceId: t.Optional(t.String())
-    }),
-    response: t.Number()
-  })
+  .guard(
+    {
+      cookie: t.Cookie({
+        deviceId: t.Optional(t.String())
+      }),
+      // Ensure deviceId is set for the following routes
+      beforeHandle({ set, cookie: { deviceId } }) {
+        if (!deviceId.value) {
+          deviceId.set({
+            value: createId(),
+            path: "/"
+          });
+        }
+        deviceId.expires = new Date(Date.now() + 365 * 24 * 60 * 60 * 1000);
+      },
+    },
+    (app) => app
+      .get("/greeting", () => "Hello Elysia!" as const, {
+        response: t.Literal("Hello Elysia!")
+      })
+      .put("/click", async ({ cookie: { deviceId } }) => {
+        await db.insert(clicks).values({
+          deviceId: deviceId.value!,
+          time: new Date()
+        });
+      },
+      {
+        response: t.Void()
+      })
+      .get("/clicks", async ({ cookie: { deviceId } }) => {
+        const select = await db.select({ count: count() })
+          .from(clicks)
+          .where(eq(clicks.deviceId, deviceId.value!));
+        return select[0].count;
+      },
+      {
+        response: t.Number()
+      })
+  )
   .listen(env.API_PORT);
 
 export type App = typeof app;
