919ff37d0ae59658466a3e90f45905fa89443c84
Elliot Nash <elliotnash@gmail.com>
2024-07-24 18:57:29 -0700

    Fix hot reload in docker

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "@solidjs/start/config";
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const hmrPorts = {
+  client: 4440,
+  server: 4441,
+  'server-function': 4442,
+}
+
 export default defineConfig({
-  vite: {
+  vite: ({ router }) => ({
     plugins: [tsconfigPaths()],
-    // server: {
-    //   hmr: {
-    //     port: 4444,
-    //   }
-    // }
-  }
+    server: {
+      hmr: {
+        port: hmrPorts[router]
+      },
+    }
+  })
 });
