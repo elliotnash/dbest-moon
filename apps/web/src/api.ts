d90c8292117d3cb7d61c268b5de62de37fbeab92
Elliot Nash <elliotnash@gmail.com>
2024-07-25 11:03:09 -0700

    Copy all ssr headers from client

--- apps/web/src/api.ts
+++ apps/web/src/api.ts
@@ -3,36 +3,26 @@ import { isServer } from "solid-js/web";
 import { parse } from "set-cookie-parser";
 import { App } from "api";
 
-const { getHeaders, setResponseHeaders } = await import("vinxi/http");
-
-// Get client headers from ssr context
-function getServerHeaders() {
-  "use server";
-  return getHeaders();
-}
-
-function setServerHeaders(headers: Headers) {
-  "use server";
-  setResponseHeaders(headers);
+let getHeaders;
+let setResponseHeaders;
+if (isServer) {
+  ({ getHeaders, setResponseHeaders } = await import("vinxi/http"));
 }
 
 export default treaty<App>('http://localhost:8080', {
   fetch: {
     credentials: 'include',
   },
-  // headers(path, options) {
-  //   // If ssr context, copy over client headers.
-  //   if (isServer) {
-  //     console.log("we're making a request server side");
-  //     // return getServerHeaders();
-  //   } else {
-  //     console.log("we're making a request client side");
-  //   }
-  // },
-  // onResponse(response) {
-  //   // If ssr context, set headers on client.
-  //   if (isServer) {
-  //     // setServerHeaders(response.headers)
-  //   }
-  // }
+  headers(path, options) {
+    // If ssr context, copy over client headers.
+    if (isServer) {
+      return getHeaders();
+    }
+  },
+  onResponse(response) {
+    // If ssr context, set headers on client.
+    if (isServer) {
+      setResponseHeaders(response.headers);
+    }
+  }
 });
